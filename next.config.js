5516b666c317233d69937ac46650a99e100ae339
Nicolaswg <nigonbol123@gmail.com>
2025-09-11 16:02:18 -0400

    Adds webpack external configuration for sanity.config.ts
    Improves the Next.js configuration by adding a custom webpack configuration that includes "sanity.config.ts" as an external dependency.
    
    This change allows for better handling of the Sanity configuration in the build process, ensuring that it is excluded from the bundle, which can improve build performance and reduce bundle size.

--- next.config.js
+++ next.config.js
@@ -4,25 +4,30 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'cdn.sanity.io',
-        port: '',
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
       },
       {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        port: '',
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
       },
       {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
-        port: '',
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
       },
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
       },
     ],
+  },
+  webpack: (config) => {
+    config.externals = config.externals || [];
+    config.externals.push("sanity.config.ts");
+    return config;
   },
 };
 
